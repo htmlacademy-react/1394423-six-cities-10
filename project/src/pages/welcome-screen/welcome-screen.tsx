b7d0d7b0f19d22078f4a6390d594c375f3bfd6f0
quickly <pika-piba@mail.ru>
2022-07-13 00:00:39 +0300

    fix eslint

--- project/src/pages/welcome-screen/welcome-screen.tsx
+++ project/src/pages/welcome-screen/welcome-screen.tsx
@@ -6,7 +6,6 @@ function WelcomeScreen(props: WelcomePageProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <Header></Header>
-
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
